a28ff0bfec4ad43a5d367112b21ae83d8b9f3047
Luc Schnell <luc.schnell@tngtech.com>
2025-09-24 20:13:12 +0200

    Add CodeSnippet styling for lang 'Go'

--- src/summaryComponents/scripts/CodeStyler.ts
+++ src/summaryComponents/scripts/CodeStyler.ts
@@ -32,6 +32,9 @@ export class CodeStyler implements Styler {
             case "bash":
                 this.styler = new GenericCodeStyler(["#"], ["minikube", "install", "podman", "kubeadm", "kubelet", "update", "containerd", "helm", "kubectl", "brew", "npm", "tsc", "terraform", "ansible", "config", "dump", "ping", "docker", "build", "push", "run", "docker-compose", "up", "down"], ["\""], charsKeys, colors);
                 break;
+            case "go":
+                this.styler = new GenericCodeStyler(["//"], ["func", "package", "import", "Println"], ["\"", "\'"], charsKeys, colors);
+                break;
             case "terraform":
                 this.styler = new GenericCodeStyler(["#"], ["depends_on", "terraform", "backend", "resource", "variable","variables", "module", "output", "provider", "data", "locals", "local", "for", "in", "if", "import", "run", "provisioner", "lifecycle", "state", "moved"], ["\""], charsKeys, colors);
                 break;
